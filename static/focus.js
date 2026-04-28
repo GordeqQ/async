5cc4c1d67e22ef0fe49b1e43c6418e2620380d86
Чернышов Всеволод Андреевич <115720168+ChernyshovVsevolod@users.noreply.github.com>
2026-04-28 17:11:45 +0500

    sendRequest changed

--- static/focus.js
+++ static/focus.js
@@ -39,19 +39,21 @@ async function run() {
 
 run();
 
-function sendRequest(url, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            if (xhr.status === 200) {
-                callback(JSON.parse(xhr.response));
+function sendRequest(url) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+                if (xhr.status === 200) {
+                    resolve(JSON.parse(xhr.response));
+                } else {
+                    reject(xhr.status);
+                }
             }
-        }
-    };
-
-    xhr.send();
+        };
+        xhr.send();
+    });
 }
 
 function reqsToMap(requisites) {
