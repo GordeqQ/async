d0c3e7c93bbc144ef72b57b191f1b7ad6bde42c3
GordeqQ <defjalox@gmail.com>
2026-04-28 17:38:57 +0500

    task 2

--- static/focus.js
+++ static/focus.js
@@ -39,21 +39,9 @@ async function run() {
 
 run();
 
-function sendRequest(url) {
-    return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.response));
-                } else {
-                    reject(xhr.status);
-                }
-            }
-        };
-        xhr.send();
-    });
+async function sendRequest(url) {
+    const response = await fetch(url);
+    return await response.json();
 }
 
 function reqsToMap(requisites) {
